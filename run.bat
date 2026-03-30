5a5bd1bb6bd451c01aa77a7044a8da50198b2130
NotMuole <thomaec2006@gmail.com>
2026-03-30 15:41:32 +0200

    Add build configs

---
+++ run.bat
@@ -0,0 +1,2 @@
+java -jar ./target/pic-simulator-1.0-SNAPSHOT.jar
+pause
---
+++ src/main/java/ausgabe.java
@@ -0,0 +1,5 @@
+public class ausgabe {
+    public static void main(String[] args) {
+        System.out.println("PIC-Simulator!");
+    }
+}
