0c5cf19fde0a40771a5558dba654b3687a7a5bf4
BuildTools <unconfigured@null.spigotmc.org>
2026-04-05 21:50:14 +0200

    Add implementations for commands

--- src/main/java/PIC/PIC16F84.java
+++ src/main/java/PIC/PIC16F84.java
@@ -32,157 +32,157 @@ public class PIC16F84 {
 
     public static void decode(int code) {
            if ((code & 16128) == 1792) {
-               log.info("ADDWF");
                int value = code & 127;
                int destination = ((code & 128) >> 7);
+               ADDWF(value, destination);
 
            } else if ((code & 16128) == 1280) {
-               log.info("ANDWF");
                int value = code & 127;
                int destination = ((code & 128) >> 7);
+               ANDWF(value, destination);
 
            } else if ((code & 16256) == 384) {
-               log.info("CLRF");
                int value = code & 127;
+               CLRF(value);
 
            } else if ((code & 16256) == 256) {
-               log.info("CLRW");
                int value_x = code & 127;
+               CLRW(value_x);
 
            } else if ((code & 16128) == 2304) {
-               log.info("COMF");
                int value = code & 127;
                int destination = ((code & 128) >> 7);
+               COMF(value, destination);
 
            } else if ((code & 16128) == 768) {
-               log.info("DECF");
                int value = code & 127;
                int destination = ((code & 128) >> 7);
+               DECF(value, destination);
 
            } else if ((code & 16128) == 2816) {
-               log.info("DECFSZ");
                int value = code & 127;
                int destination = ((code & 128) >> 7);
+               DECFSZ(value, destination);
 
            } else if ((code & 16128) == 2560) {
-               log.info("INCF");
                int value = code & 127;
                int destination = ((code & 128) >> 7);
+               INCF(value, destination);
 
            } else if ((code & 16128) == 3840) {
-               log.info("INCFSZ");
                int value = code & 127;
                int destination = ((code & 128) >> 7);
+               INCFSZ(value, destination);
 
            } else if ((code & 16128) == 1024) {
-               log.info("IORWF");
                int value = code & 127;
                int destination = ((code & 128) >> 7);
+               IORWF(value, destination);
 
            } else if ((code & 16128) == 2048) {
-               log.info("MOVF");
                int value = code & 127;
                int destination = ((code & 128) >> 7);
+               MOVF(value, destination);
 
            } else if ((code & 16256) == 128) {
-               log.info("MOVWF");
                int value = code & 127;
+               MOVWF(value);
 
            } else if ((code & 16287) == 0) {
-                log.info("NOP");
+                NOP();
 
            } else if ((code & 16128) == 3328) {
-               log.info("RLF");
                int value = code & 127;
                int destination = ((code & 128) >> 7);
+               RLF(value, destination);
 
            } else if ((code & 16128) == 3072) {
-               log.info("RRF");
                int value = code & 127;
                int destination = ((code & 128) >> 7);
+               RRF(value, destination);
 
            } else if ((code & 16128) == 512) {
-               log.info("SUBWF");
                int value = code & 127;
                int destination = ((code & 128) >> 7);
+               SUBWF(value, destination);
 
            } else if ((code & 16128) == 3584) {
-               log.info("SWAPF");
                int value = code & 127;
                int destination = ((code & 128) >> 7);
+               SWAPF(value, destination);
 
            } else if ((code & 16128) == 1536) {
-               log.info("XORWF");
                int value = code & 127;
                int destination = ((code & 128) >> 7);
+               XORWF(value, destination);
 
            } else if ((code & 15360) == 4096) {
-               log.info("BCF");
                int value = code & 127;
                int destination = ((code & 896) >> 7);
+               BCF(value, destination);
 
            } else if ((code & 15360) == 5120) {
-               log.info("BSF");
                int value = code & 127;
                int destination = ((code & 896) >> 7);
+               BSF(value, destination);
 
            } else if ((code & 15360) == 6144) {
-               log.info("BTFSC");
                int value = code & 127;
                int destination = ((code & 896) >> 7);
+               BTFSC(value, destination);
 
            } else if ((code & 15360) == 7168) {
-               log.info("BTFSS");
                int value = code & 127;
                int destination = ((code & 896) >> 7);
+               BTFSS(value, destination);
 
            } else if ((code & 15872) == 15872) {
-               log.info("ADDLW");
                int value = code & 255;
+               ADDLW(value);
 
            } else if ((code & 16128) == 14592) {
-               log.info("ANDLW");
                int literal = code & 255;
+               ANDLW(literal);
 
            } else if ((code & 14336) == 8192) {
-               log.info("CALL");
                int literal = code & 2047;
+               CALL(literal);
 
            } else if ((code & 16383) == 100) {
-               log.info("CLRWDT");
+               CLRWDT();
 
            } else if ((code & 14336) == 10240) {
-               log.info("GOTO");
                int literal = code & 2047;
+               GOTO(literal);
 
            } else if ((code & 16128) == 14336) {
-               log.info("IORLW");
                int literal = code & 255;
+               IORLW(literal);
 
            } else if ((code & 15360) == 12288) {
-               log.info("MOVLW");
                int literal = code & 255;
+               MOVLW(literal);
 
            } else if ((code & 16383) == 9) {
-               log.info("RETFIE");
+               RETFIE();
 
            } else if ((code & 15360) == 13312) {
-               log.info("RETILW");
                int literal = code & 255;
+               RETILW(literal);
 
            } else if ((code & 16383) == 8) {
-               log.info("RETURN");
+               RETURN();
 
            } else if ((code & 16383) == 99) {
-               log.info("SLEEP");
+               SLEEP();
 
            } else if ((code & 15872) == 15360) {
-               log.info("SUBLW");
                int literal = code & 255;
+               SUBLW(literal);
 
            } else if ((code & 16128) == 14848) {
-               log.info("XORLW");
                int literal = code & 255;
+               XORLW(literal);
            }
     }
 
@@ -276,5 +276,144 @@ public class PIC16F84 {
         this.Programcounter = 0;
     }
 
+    public static void ADDWF(int value, int destination) {
+        log.info("ADDWF");
+    }
+
+    public static void ANDWF(int value, int destination) {
+        log.info("ANDWF");
+    }
+
+    public static void CLRF(int value) {
+        log.info("CLRF");
+    }
+
+    public static void CLRW(int value) {
+        log.info("CLRW");
+    }
+
+    public static void COMF(int value, int destination) {
+        log.info("COMF");
+    }
+
+    public static void DECF(int value, int destination) {
+        log.info("DECF");
+    }
+
+    public static void DECFSZ(int value, int destination) {
+        log.info("DECFSZ");
+    }
+
+    public static void INCF(int value, int destination) {
+        log.info("INCF");
+    }
+
+    public static void INCFSZ(int value, int destination) {
+        log.info("INCFSZ");
+    }
+
+    public static void IORWF(int value, int destination) {
+        log.info("IORWF");
+    }
+
+    public static void MOVF(int value, int destination) {
+        log.info("MOVF");
+    }
+
+    public static void MOVWF(int value) {
+        log.info("MOVWF");
+    }
+
+    public static void NOP() {
+        log.info("NOP");
+    }
+
+    public static void RLF(int value, int destination) {
+        log.info("RLF");
+    }
+
+    public static void RRF(int value, int destination) {
+        log.info("RRF");
+    }
+
+    public static void SUBWF(int value, int destination) {
+        log.info("SUBWF");
+    }
+
+    public static void SWAPF(int value, int destination) {
+        log.info("SWAPF");
+    }
+
+    public static void XORWF(int value, int destination) {
+        log.info("XORWF");
+    }
+
+    public static void BCF(int value, int destination) {
+        log.info("BCF");
+    }
+
+    public static void BSF(int value, int destination) {
+        log.info("BSF");
+    }
+
+    public static void BTFSC(int value, int destination) {
+        log.info("BTFSC");
+    }
+
+    public static void BTFSS(int value, int destination) {
+        log.info("BTFSS");
+    }
+
+    public static void ADDLW(int value) {
+        log.info("ADDLW");
+    }
+
+    public static void ANDLW(int literal) {
+        log.info("ANDLW");
+    }
+
+    public static void CALL(int literal) {
+        log.info("CALL");
+    }
+
+    public static void CLRWDT() {
+        log.info("CLRWDT");
+    }
+
+    public static void GOTO(int literal) {
+        log.info("GOTO");
+    }
+
+    public static void IORLW(int literal) {
+        log.info("IORLW");
+    }
+
+    public static void MOVLW(int literal) {
+        log.info("MOVLW");
+    }
+
+    public static void RETFIE() {
+        log.info("RETFIE");
+    }
+
+    public static void RETILW(int literal) {
+        log.info("RETILW");
+    }
+
+    public static void RETURN() {
+        log.info("RETURN");
+    }
+
+    public static void SLEEP() {
+        log.info("SLEEP");
+    }
+
+    public static void SUBLW(int literal) {
+        log.info("SUBLW");
+    }
+
+    public static void XORLW(int literal) {
+        log.info("XORLW");
+    }
 
 }
