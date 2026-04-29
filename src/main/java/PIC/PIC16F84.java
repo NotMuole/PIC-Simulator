367a167c188572bed64875ec7f4dd26df7cb6962
Luca Neils <luca.neils@zimmer-group.com>
2026-04-29 11:03:21 +0200

    removed hardwareansteuerung and implemented convert jar to exe

--- src/main/java/PIC/PIC16F84.java
+++ src/main/java/PIC/PIC16F84.java
@@ -5,8 +5,6 @@ import UI.MainFrame;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import Hardwareansteuerung.Hardwareansteuerung;
-
 import java.util.List;
 import java.util.ArrayList;
 
@@ -55,10 +53,7 @@ public class PIC16F84 {
     public static boolean isSleep = false;
     private static final Logger log = LogManager.getLogger(PIC16F84.class);
 
-    public PIC16F84() {
-        Hardwareansteuerung.initialize();
-        Hardwareansteuerung.print(0, 0, 31, 0);
-    }
+    public PIC16F84() {}
 
     public static List<Integer> decodeAddress(int address) {
         if (rp0 == 1 && address < 128) address += 128;
